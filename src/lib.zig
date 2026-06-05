cc35a88f825cd9894219792ece138999cc0b8ec3
Meghan Denny <hello@nektro.net>
2026-06-04 19:40:07 -0700

    update to zig 0.16.0

--- src/lib.zig
+++ src/lib.zig
@@ -54,7 +54,7 @@ pub fn fmtValueLiteral(w: anytype, value: anytype, print_type_name: bool) !void 
         .void => {
             try w.writeAll("{}");
         },
-        .optional => |_| {
+        .optional => {
             if (value) |cap| {
                 try fmtValueLiteral(w, cap, print_type_name);
             } else {
